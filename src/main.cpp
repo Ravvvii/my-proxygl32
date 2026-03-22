43e2b730f9e0d65516b36657a20ed4304fccda57
Ravvvii <leew2918@gmail.com>
2026-03-22 22:09:30 +0700

    new

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,16 @@
 #include <Windows.h>
 #include <iostream>
-
 #include "proxy_gl32.h"
 
-// The thread function that will run once the DLL is injected
 DWORD WINAPI MainThread(LPVOID lpParam)
 {
-    AllocConsole();
-    FILE* f;
-    freopen_s(&f, "CONOUT$", "w", stdout);
-    freopen_s(&f, "CONOUT$", "w", stderr);
+    Beep(750, 300); 
+    Beep(1000, 300);
 
-    std::cout << "[+] DLL Injected successfully!" << std::endl;
+    MessageBoxA(NULL, "ProxyGL32: Gacor Terinject!", "Status Mod", MB_OK | MB_ICONINFORMATION);
 
-    // Cleanup
-    if (f) fclose(f);
-    FreeConsole();
-
-    // Unload the DLL and exit the thread safely
-    FreeLibraryAndExitThread((HMODULE)lpParam, 0);
     return 0;
 }
-
-
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
@@ -31,11 +19,8 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
         proxy_gl32::setup_proxy();
 
         DisableThreadLibraryCalls(hinstDLL);
-        CreateThread(nullptr, 0, MainThread, hinstDLL, 0, nullptr);
-
-    } else if (fdwReason == DLL_PROCESS_DETACH)
-    {
-        std::cout << "unloading" << std::endl;
+        
+        CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)MainThread, hinstDLL, 0, nullptr);
     }
     return TRUE;
 }
